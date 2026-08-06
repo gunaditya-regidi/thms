3e026079984d5571aad589e78bd017f0a2e526ef
Gunaditya Regidi <regidigunaditya@gmail.com>
2026-08-06 21:10:14 +0530

    Remove scan to login feature and implement view current password option in all account dashboards

--- models.py
+++ models.py
@@ -10,11 +10,13 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
+    plain_password = db.Column(db.String(255), nullable=True)
     role = db.Column(db.String(20), nullable=False) # 'admin', 'manager', 'team_leader'
     is_active = db.Column(db.Boolean, default=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
+        self.plain_password = password
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
